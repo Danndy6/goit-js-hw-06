6a089f75a8754748b16ba397a5db410b02345a4b
Danndy6 <danndy754@gmail.com>
2021-12-03 16:50:15 +0200

    JS_HW_06_WIP

---
+++ js/task-02.js
@@ -0,0 +1,17 @@
+const ingredients = [
+  'Potatoes',
+  'Mushrooms',
+  'Garlic',
+  'Tomatos',
+  'Herbs',
+  'Condiments',
+];
+
+const lisdtItem = (ingredients) => ingredients.map(element => {
+  const item = document.createElement('li')
+  item.className = ('item')
+  item.textContent = element
+  return item
+});
+const itemLi = document.querySelector('#ingredients');
+itemLi.append(...lisdtItem(ingredients));
