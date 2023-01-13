05360bb1202fa9034e24f935742d43403df90a8b
Danndy6 <danndy754@gmail.com>
2023-01-13 16:23:24 +0200

    done

---
+++ js/task-07.js
@@ -0,0 +1,13 @@
+const refs = {
+    rangeInput: document.querySelector('#font-size-control'),
+    textBelongInput: document.querySelector('#text')
+};
+
+    refs.textBelongInput.style.fontSize = `${refs.rangeInput.value}px`;
+
+const onRangeChange = (event) => {
+    const { value } = event.target;
+    refs.textBelongInput.style.fontSize = `${value}px`;
+};
+
+refs.rangeInput.addEventListener('input', onRangeChange);
